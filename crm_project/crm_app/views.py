3512c8752928d4c60b461bce4889b87495504ebb
Zubairahmdfaraz <96882601+Zubairahmdfaraz@users.noreply.github.com>
2023-09-02 18:12:40 +0530

    Add files via upload

---
+++ crm_project/crm_app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Customer
+from .forms import CustomerForm
+
+def customer_list(request):
+    customers = Customer.objects.all()
+    return render(request, 'crm_app/customer_list.html', {'customers': customers})
+
+def customer_add(request):
+    if request.method == 'POST':
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('customer_list')
+    else:
+        form = CustomerForm()
+    return render(request, 'crm_app/customer_form.html', {'form': form})
+
+def customer_edit(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, instance=customer)
+        if form.is_valid():
+            form.save()
+            return redirect('customer_list')
+    else:
+        form = CustomerForm(instance=customer)
+    return render(request, 'crm_app/customer_form.html', {'form': form})
+
+def customer_delete(request, pk):
+    customer = get_object_or_404(Customer, pk=pk)
+    if request.method == 'POST':
+        customer.delete()
+        return redirect('customer_list')
+    return render(request, 'crm_app/customer_confirm_delete.html', {'customer': customer})
+
